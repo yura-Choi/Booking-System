d496d9edf644223396f9de0fe8117825b6084db9
yura-Choi <dbfk1207@gmail.com>
2017-01-31 20:30:46 +0900

    [Need correction] page moving error

--- resource/js/join-continue.js
+++ resource/js/join-continue.js
@@ -2,14 +2,12 @@ $("#generalbtn").bind("click", function(){
     $.post("/join/select/type",
         function(result){
             if (result == "success"){
-                event.preventDefault();
-                window.location="join_inputinfo.html";
+                location.href="join_inputinfo.html";
             }
     });
 
 });
 
 $("#administratorbtn").bind("click", function(){
-    event.preventDefault();
-    window.location="join_administrator.html";
+    location.href="join_administrator.html";
 });
--- resource/js/sessionlogin.js
+++ resource/js/sessionlogin.js
@@ -4,19 +4,19 @@ $(function(){
         function(result, status){
             if(result != ""){
                 $("#navlogin, #loginbtn").text("Logout")
-                                         .attr("href", "");
-                $("#navlogin, #loginbtn").click(function(){
-                    $.post("/index/logout",
-                        function(){
-                            location.href="index.html";
-                        }
-                    );
-                    
-                });
+                                         .attr("href", "index.html");
             } else if(result == "") {
                 $("#navlogin, #loginbtn").text("Login")
                                          .attr("href", "login.html");
             }
         }
     );
+});
+
+$("#navlogin, #loginbtn").click(function(){
+    $.post("/index/logout",
+        function(){
+            location.href="index.html";
+        }
+    );                    
 });
